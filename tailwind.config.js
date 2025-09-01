3b13554471b4a17124a209f5eeb9663c0c26a845
anamul01988 <anamulhaque0198877@gmail.com>
2025-09-02 00:38:14 +0600

    project initialized

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+import withMT from "@material-tailwind/react/utils/withMT";
+
+export default withMT({
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primaryRed: "#e11d48", // 🔥 your custom red
+      },
+    },
+  },
+  plugins: [],
+});
