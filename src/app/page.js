ca807c9e603465253bc3536f3b14afeb171b3c56
anamul01988 <anamulhaque0198877@gmail.com>
2025-09-03 02:00:39 +0600

    modal design done

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,12 @@
 "use client";
 
-import {
-  // Button,
-  // Drawer,
-  // Modal,
-  // Card,
-  // Input,
-  // Navbar,
-  Footer,
-} from "@/components/ui";
+import HomePage from "@/components/pages/home/Home";
+import { Footer } from "@/components/ui";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-6 bg-gray-100">
-      {/* Footer */}
+      <HomePage />
       <Footer />
     </main>
   );
