922ac69da53a35979241900cd5933db8f5ceb8c7
Md. Asiquzzaman <asiquzzaman@thekowcompany.com>
2025-09-06 23:03:23 +0600

    demo_html done

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import { LandingPage } from "@/components/_components/LandingPage";
 import HomePage from "@/components/pages/home/Home";
 import { Footer } from "@/components/ui";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-6 bg-gray-100">
+      <LandingPage/>
       <HomePage />
       <Footer />
     </main>
