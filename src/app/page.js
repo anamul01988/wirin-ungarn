72889ba0336d86c4f8303b95223d1f7e4cab0483
anamul01988 <anamulhaque0198877@gmail.com>
2025-09-07 01:40:21 +0600

    bg image issue solve

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,16 @@ import { Footer } from "@/components/ui";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center gap-6 bg-gray-100 main__page">
+    <div
+      className="flex flex-col items-center justify-center gap-6 bg-gray-100 main__page"
+      style={{
+        backgroundImage: 'url("/assets/pattern-bg.jpg")',
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        minHeight: "100vh",
+        width: "100%",
+      }}
+    >
       <LandingPage />
       <HomePage />
       {/* <Footer /> */}
--- src/components/_components/LandingPage.js
+++ src/components/_components/LandingPage.js
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import "./style.css";
 import { useRouter } from "next/navigation";
 import ImpressumtModal from "../ui/ImpressumModal";
+import HomePage from "../pages/home/Home";
 
 const LandingPage = () => {
   const [tickerClosed, setTickerClosed] = useState(false);
