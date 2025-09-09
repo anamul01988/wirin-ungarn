4787abfe077d9e86ace2a9005de8795d66ae40c1
anamul01988 <anamulhaque0198877@gmail.com>
2025-09-10 00:15:17 +0600

    added font opensans & roboto condensed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,14 @@
 "use client";
-import { Geist, Geist_Mono } from "next/font/google";
+import {
+  Geist,
+  Geist_Mono,
+  Open_Sans,
+  Roboto_Condensed,
+} from "next/font/google";
 import "./globals.css";
 
 import { ThemeProvider } from "@material-tailwind/react";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -13,11 +19,29 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const openSans = Open_Sans({
+  variable: "--font-open-sans",
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+const robotoCondensed = Roboto_Condensed({
+  variable: "--font-roboto-condensed",
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`
+          ${geistSans.variable} 
+          ${geistMono.variable} 
+          ${openSans.variable} 
+          ${robotoCondensed.variable} 
+          antialiased
+        `}
       >
         <ThemeProvider>{children}</ThemeProvider>
       </body>
