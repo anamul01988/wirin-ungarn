bf0aa64fdb165e011f546a1e8a358ac9644e03ef
ashikfifa <ashikahmedfifa@gmail.com>
2025-10-11 00:30:22 +0600

    deployment error fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
