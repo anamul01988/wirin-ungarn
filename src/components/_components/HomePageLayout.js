8e5e22dd062e3a013fde272cdfb96ab7e98ef7c8
anamul01988 <anamulhaque0198877@gmail.com>
2025-11-15 21:18:13 +0600

    merged

--- src/components/_components/HomePageLayout.js
+++ src/components/_components/HomePageLayout.js
@@ -180,7 +180,6 @@ export default function HomePageLayout() {
       </header>
       <LandingPage />
       {/* <HomePage /> */}
-      {/* <Footer /> */}
 
       {/* Modals */}
       <LoginModal
