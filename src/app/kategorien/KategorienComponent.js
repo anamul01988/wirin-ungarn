690cf9914f81a60a6a3e66ce899ec028756ab9e4
anamul01988 <anamulhaque0198877@gmail.com>
2025-11-17 01:43:13 +0600

    kategorien page modified for breadcrumb

--- src/app/kategorien/KategorienComponent.js
+++ src/app/kategorien/KategorienComponent.js
@@ -49,11 +49,11 @@ export default function KategorienComponent() {
 
         <DialogBody className="overflow-auto custom__modal_area flex-1 pl-4 mr-1 my-1">
           {/* Breadcrumb */}
-          <div className="mb-4 px-0 pt-2">
+          {/* <div className="mb-4 px-0 pt-2">
             <Breadcrumb className="text-sm" />
-          </div>
-          
-          <div className="mt-8">
+          </div> */}
+
+          <div className="">
             <KategorienPage />
           </div>
         </DialogBody>
@@ -71,4 +71,3 @@ export default function KategorienComponent() {
     </>
   );
 }
-
